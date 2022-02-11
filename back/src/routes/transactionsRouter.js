68a888414e43186e14b94da19624ced19aa55157
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-02-11 14:32:26 -0300

    Preparando revisão de código

--- back/src/routes/transactionsRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getTransactions, registerTransaction } from '../controllers/transactionsController.js';
-
-const messageRouter = Router();
-
-messageRouter.post('/transactions', registerTransaction);
-messageRouter.get('/transactions', getTransactions);
-export default messageRouter;
